5033bc369e4a0c563fd20b8a312d3232b1de642f
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2018-07-13 11:51:13 +0200

    Basic features ok

--- sample/test.js
+++ sample/test.js
@@ -3,17 +3,15 @@
 "use strict" ;
 
 var Promise = require( 'seventh' ) ;
-var Queue = require( '..' ) ;
-var Job = Queue.Job ;
+var qKit = require( '..' ) ;
+var Queue = qKit.Queue ;
+var Job = qKit.Job ;
 
 
-var queue = new Queue( { concurrency: 2 } ) ;
+var queue = new Queue( { concurrency: 3 } ) ;
 
 function jobFn( data ) {
-	console.log( "Starting job #" + data.id ) ;
-	return Promise.resolveTimeout( data.timeout ).then( () => {
-		console.log( "Done job #" + data.id ) ;
-	} ) ;
+	return Promise.resolveTimeout( data.timeout ) ;
 }
 
 var count = 0 ;
@@ -23,8 +21,38 @@ queue.add( new Job( jobFn , { id: count ++ , timeout: 3000 } ) ) ;
 queue.add( new Job( jobFn , { id: count ++ , timeout: 300 } ) ) ;
 queue.add( new Job( jobFn , { id: count ++ , timeout: 300 } ) ) ;
 queue.add( new Job( jobFn , { id: count ++ , timeout: 3000 } ) ) ;
+queue.add( new Job( jobFn , { id: count ++ , timeout: 5000 } ) ) ;
+queue.add( new Job( jobFn , { id: count ++ , timeout: 500 } ) ) ;
+queue.add( new Job( jobFn , { id: count ++ , timeout: 2000 } ) ) ;
 queue.add( new Job( jobFn , { id: count ++ , timeout: 300 } ) ) ;
 queue.add( new Job( jobFn , { id: count ++ , timeout: 300 } ) ) ;
 
+queue.on( 'stopped' , job => console.log( "Queue is stopped" ) ) ;
+queue.on( 'started' , job => console.log( "Queue is started" ) ) ;
+queue.on( 'idling' , job => console.log( "Queue is idling" ) ) ;
+queue.on( 'running' , job => console.log( "Queue is running" ) ) ;
+queue.on( 'jobStart' , job => console.log( "Starting job #" + job.data.id + " (" + job.data.timeout + "ms)" ) ) ;
+queue.on( 'jobDone' , job => console.log( "Done job #" + job.data.id + " (" + job.data.timeout + "ms)" ) ) ;
+
 queue.start() ;
+
+/*
+setInterval( () => {
+	queue.add( new Job( jobFn , { id: count ++ , timeout: 300 } ) ) ;
+	queue.add( new Job( jobFn , { id: count ++ , timeout: 3000 } ) ) ;
+} , 500 ) ;
+//*/
+
+setTimeout( () => queue.stop() , 1000 ) ;
+setTimeout( () => queue.start() , 10000 ) ;
+
+setTimeout( () => {
+	queue.add( new Job( jobFn , { id: count ++ , timeout: 2700 } ) ) ;
+	queue.add( new Job( jobFn , { id: count ++ , timeout: 700 } ) ) ;
+} , 8000 ) ;
+
+setTimeout( () => {
+	queue.add( new Job( jobFn , { id: count ++ , timeout: 2800 } ) ) ;
+	queue.add( new Job( jobFn , { id: count ++ , timeout: 800 } ) ) ;
+} , 15000 ) ;
 

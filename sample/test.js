1175420619cf1e1ac15d3779e24ccac90e6cf137
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2018-07-13 17:29:58 +0200

    Clean up, tasks, progress event

--- sample/test.js
+++ sample/test.js
@@ -8,7 +8,10 @@ var Queue = qKit.Queue ;
 var Job = qKit.Job ;
 
 
-var queue = new Queue( { concurrency: 3 } ) ;
+var queue = new Queue( {
+	concurrency: 3 ,
+//	cleanUpInterval: 5000
+} ) ;
 
 function jobFn( data ) {
 	return Promise.resolveTimeout( data.timeout ) ;
@@ -16,16 +19,16 @@ function jobFn( data ) {
 
 var count = 0 ;
 
-queue.add( new Job( jobFn , { id: count ++ , timeout: 300 } ) ) ;
-queue.add( new Job( jobFn , { id: count ++ , timeout: 3000 } ) ) ;
-queue.add( new Job( jobFn , { id: count ++ , timeout: 300 } ) ) ;
-queue.add( new Job( jobFn , { id: count ++ , timeout: 300 } ) ) ;
-queue.add( new Job( jobFn , { id: count ++ , timeout: 3000 } ) ) ;
-queue.add( new Job( jobFn , { id: count ++ , timeout: 5000 } ) ) ;
-queue.add( new Job( jobFn , { id: count ++ , timeout: 500 } ) ) ;
-queue.add( new Job( jobFn , { id: count ++ , timeout: 2000 } ) ) ;
-queue.add( new Job( jobFn , { id: count ++ , timeout: 300 } ) ) ;
-queue.add( new Job( jobFn , { id: count ++ , timeout: 300 } ) ) ;
+queue.addJob( jobFn , { id: count ++ , timeout: 300 } ) ;
+queue.addJob( jobFn , { id: count ++ , timeout: 3000 } ) ;
+queue.addJob( jobFn , { id: count ++ , timeout: 300 } ) ;
+queue.addJob( jobFn , { id: count ++ , timeout: 300 } ) ;
+queue.addJob( jobFn , { id: count ++ , timeout: 3000 } ) ;
+queue.addJob( jobFn , { id: count ++ , timeout: 5000 } ) ;
+queue.addJob( jobFn , { id: count ++ , timeout: 500 } ) ;
+queue.addJob( jobFn , { id: count ++ , timeout: 2000 } ) ;
+queue.addJob( jobFn , { id: count ++ , timeout: 300 } ) ;
+queue.addJob( jobFn , { id: count ++ , timeout: 300 } ) ;
 
 queue.on( 'stopped' , () => console.log( "Queue is stopped" ) ) ;
 queue.on( 'started' , () => console.log( "Queue is started" ) ) ;
@@ -33,13 +36,14 @@ queue.on( 'idling' , () => console.log( "Queue is idling" ) ) ;
 queue.on( 'running' , () => console.log( "Queue is running" ) ) ;
 queue.on( 'jobStart' , job => console.log( "Starting job #" + job.data.id + " (" + job.data.timeout + "ms)" ) ) ;
 queue.on( 'jobDone' , job => console.log( "Done job #" + job.data.id + " (" + job.data.timeout + "ms)" ) ) ;
+queue.on( 'progress' , ( jobDone , jobInProgress , jobRemaining ) => console.log( "Progress" , jobDone , jobInProgress , jobRemaining ) ) ;
 
 queue.start() ;
 
 /*
 setInterval( () => {
-	queue.add( new Job( jobFn , { id: count ++ , timeout: 300 } ) ) ;
-	queue.add( new Job( jobFn , { id: count ++ , timeout: 3000 } ) ) ;
+	queue.addJob( jobFn , { id: count ++ , timeout: 300 } ) ;
+	queue.addJob( jobFn , { id: count ++ , timeout: 3000 } ) ;
 } , 500 ) ;
 //*/
 
@@ -47,13 +51,13 @@ setTimeout( () => queue.stop() , 1000 ) ;
 setTimeout( () => queue.start() , 10000 ) ;
 
 setTimeout( () => {
-	queue.add( new Job( jobFn , { id: count ++ , timeout: 2700 } ) ) ;
-	queue.add( new Job( jobFn , { id: count ++ , timeout: 700 } ) ) ;
+	queue.addJob( jobFn , { id: count ++ , timeout: 2700 } ) ;
+	queue.addJob( jobFn , { id: count ++ , timeout: 700 } ) ;
 } , 8000 ) ;
 
 setTimeout( () => {
-	queue.add( new Job( jobFn , { id: count ++ , timeout: 2800 } ) ) ;
-	queue.add( new Job( jobFn , { id: count ++ , timeout: 800 } ) ) ;
+	queue.addJob( jobFn , { id: count ++ , timeout: 2800 } ) ;
+	queue.addJob( jobFn , { id: count ++ , timeout: 800 } ) ;
 } , 15000 ) ;
 
 
